1d3e04a0cb9370778afa1195e8c79229b02962bb
Gordon Doskas <gdoskas2012@email.phoenix.edu>
2015-12-11 12:14:31 -0500

    complete setup Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,15 @@
 
 // grab our gulp packages
 var gulp = require('gulp'),
-    jshint = require('gulp-jshint');
+    jshint = require('gulp-jshint'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps');
 
 // define the default task and add the watch task to it
 gulp.task('default', ['watch']);
+
+
+// TASKS
 
 // configure the jshint task
 gulp.task('jshint', function()
@@ -15,8 +20,34 @@ gulp.task('jshint', function()
     .pipe(jshint.reporter('jshint-stylish'));
 });
 
+// configure the sass task
+gulp.task('build-css', function()
+{
+  return gulp.src('source/scss/**/*.scss')
+    .pipe(sourcemaps.init())
+      .pipe(sass())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('public/assets/stylesheets'));
+});
+
+// task: build-js
+gulp.task('build-js', function()
+{
+  return gulp.src('source/javascript/**/*.js')
+    .pipe(sourcemaps.init())
+      .pipe(concat('bundle.js'))
+      // only ugglify if gulp is ran with '--type production'
+      .pipe(gutil.env.type === 'production' ? uglify() : gutil.noop())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('public/assets/javascript'));
+});
+
+
+// WATCHES
+
 // configure which files to watch and what tasks to use on file changes
 gulp.task('watch', function()
 {
   gulp.watch('source/javascript/**/*.js', ['jshint']);
+  gulp.watch('source/scss/**/*scss', ['build-css']);
 });
