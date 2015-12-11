78554c9af3cae4f5988ee628a02164cbfecd2202
Gordon Doskas <gdoskas2012@email.phoenix.edu>
2015-12-11 01:42:13 -0500

    Transfer from CodePen. add index.html script.js style.css

---
+++ script.js
@@ -0,0 +1,79 @@
+$(".num").on("click", function()
+{
+  if($(this).hasClass("answered"))
+  {
+    clearAll();
+    $(".btn").removeClass("answered");
+  }
+
+  onNum($(this).html());
+});
+
+$(".op").on("click", function()
+{
+  onOp($(this).html());
+});
+
+$(".equals").on("click", function()
+{
+  equals();
+});
+
+
+
+
+function onClear()
+{
+  $("#calc-cur").html("");
+}
+
+function clearAll()
+{
+  onClear();
+  $("#calc-acc").html("");
+}
+
+
+function onNum(num)
+{
+
+  /*$(this).addClass("answered");*/
+ /* hide(this);*/
+
+
+
+  /*if($(this).hasClass("answered"))
+  {
+    clearAll();
+    $(".btn-success").removeClass("answered");
+  }*/
+
+
+  $("#calc-cur").html($("#calc-cur").html() + num);
+}
+
+function onOp(op)
+{
+  var num = $("#calc-cur").html();
+  /*$("#calc-acc").html($("#calc-acc").html() + " " + num + " " + op);*/
+  $("#calc-acc").html($("#calc-acc").html() + num + op);
+  onClear();
+  /*data.push(num);
+  data.push(op);
+  updateData();*/
+}
+
+function equals()
+{
+  onOp("");
+
+  var expr = $("#calc-acc").html();
+  var ans = eval(expr);
+
+  $("#calc-acc").html(expr + "=" + ans);
+  $("#calc-cur").html(ans);
+
+  $(".btn").addClass("answered");
+  //console.log("+ANS");
+
+}
