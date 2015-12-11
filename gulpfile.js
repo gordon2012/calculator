ea448a133bb0e3e1e389194c1975b7adee8be0af
Gordon Doskas <gdoskas2012@email.phoenix.edu>
2015-12-11 10:56:49 -0500

    Continue setup Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,22 @@
+// File: gulpfile.js
+
 // grab our gulp packages
-
 var gulp = require('gulp'),
-    gutils = require('gulp-util');
+    jshint = require('gulp-jshint');
 
+// define the default task and add the watch task to it
+gulp.task('default', ['watch']);
 
-gulp.task('default', function()
+// configure the jshint task
+gulp.task('jshint', function()
 {
-  return gutils.log('Gulp is running!');
+  return gulp.src('source/javascript/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+// configure which files to watch and what tasks to use on file changes
+gulp.task('watch', function()
+{
+  gulp.watch('source/javascript/**/*.js', ['jshint']);
 });
